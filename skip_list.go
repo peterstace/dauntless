ca227230f812047bbadfae9f765258572e5466a1
Peter Stace <peterstace@gmail.com>
2017-03-24 20:54:02 +1100

    Make tests better

--- skip_list.go
+++ skip_list.go
@@ -4,6 +4,7 @@ import "math/rand"
 
 type skipList struct {
 	header *element
+	rnd    *rand.Rand
 }
 
 type element struct {
@@ -12,14 +13,15 @@ type element struct {
 	next   []*element
 }
 
-func newSkipList(levels int) skipList {
+func newSkipList(levels int) *skipList {
 	assert(levels >= 1)
-	return skipList{
+	return &skipList{
 		header: &element{
 			offset: -1,
 			data:   "__HEADER__",
 			next:   make([]*element, levels),
 		},
+		rnd: rand.New(rand.NewSource(3)),
 	}
 }
 
@@ -27,7 +29,7 @@ func (s *skipList) insert(offset int, data string) {
 
 	// TODO: Selecting height could be more efficient.
 	height := 1
-	for rand.Int()%2 == 0 && height+1 < len(s.header.next) {
+	for s.rnd.Int()%2 == 0 && height < len(s.header.next) {
 		height++
 	}
 
@@ -37,9 +39,12 @@ func (s *skipList) insert(offset int, data string) {
 	level := height - 1
 	for {
 
-		if offset == root.offset {
-			break
-		}
+		assert(root != nil)
+
+		// The line MUST NOT exist before being inserted. Before calling
+		// insert, check to see if it exists using find (also check for any
+		// data being changed at the same time).
+		assert(root == s.header || offset >= root.offset+len(root.data))
 
 		rightOfRoot := root.next[level]
 
@@ -49,18 +54,15 @@ func (s *skipList) insert(offset int, data string) {
 			}
 			if level > 0 {
 				level--
-				continue
 			} else {
-				break
+				return
 			}
+		} else {
+			root = rightOfRoot
 		}
-		root = rightOfRoot
 	}
 }
 
 func (s *skipList) find(offset int) *element {
 	return nil
-}
-
-func (s *skipList) remove(count int) {
 }
--- skip_list_test.go
+++ skip_list_test.go
@@ -2,89 +2,106 @@ package main
 
 import "testing"
 
-func TestSkipList_SingleInsert(t *testing.T) {
-
-	s := newSkipList(1)
-	s.insert(0, "0123")
-
-	e := s.header.next[0]
-	if !(e != nil) {
-		t.Fatal()
-	}
-	if !(len(e.next) == 1) {
-		t.Fatal()
-	}
-	if !(e.next[0] == nil) {
-		t.Fatal()
-	}
-	if !(e.offset == 0) {
-		t.Fatal()
-	}
-	if !(e.data == "0123") {
+func Expect(t *testing.T, b bool) {
+	if !b {
 		t.Fatal()
 	}
 }
 
-func TestSkipList_DoubleInsert(t *testing.T) {
+type content struct {
+	offset int
+	data   string
+}
+
+func SanityCheck(t *testing.T, s *skipList, contents []content) {
+
+	var elements []*element
+	for e := s.header.next[0]; e != nil; e = e.next[0] {
+		elements = append(elements, e)
+	}
+
+	i := 0
+	for _, e := range elements {
+		if contents[i].offset != e.offset {
+			t.Errorf("Idx=%d WantOffset=%d GotOffset=%d", i, contents[i].offset, e.offset)
+		}
+		if contents[i].data != e.data {
+			t.Errorf("Idx=%d WantData=%q GotData=%q", i, contents[i].data, e.data)
+		}
+		i++
+	}
+
+	for level := range s.header.next {
+		lastI := -1
+		for e := s.header.next[level]; e != nil; e = e.next[level] {
+			foundI := -1
+			for i := range elements {
+				if elements[i] == e {
+					foundI = i
+					break
+				}
+			}
+			if foundI == -1 {
+				t.Errorf("Could not find node")
+			}
+			if foundI <= lastI {
+				t.Errorf("Node didn't advance")
+			}
+			lastI = foundI
+		}
+	}
+}
+
+func TestSkipList_01_Empty(t *testing.T) {
+	s := newSkipList(1)
+	SanityCheck(t, s, []content{})
+}
+
+func TestSkipList_01_SingleInsert(t *testing.T) {
+
+	s := newSkipList(1)
+	s.insert(0, "0123")
+
+	SanityCheck(t, s, []content{
+		{0, "0123"},
+	})
+}
+
+func TestSkipList_01_DoubleInsert(t *testing.T) {
 
 	s := newSkipList(1)
 	s.insert(0, "0123")
 	s.insert(4, "4567")
 
-	e1 := s.header.next[0]
-	if !(e1 != nil) {
-		t.Fatal()
-	}
-	if !(len(e1.next) == 1) {
-		t.Fatal()
-	}
-
-	e2 := e1.next[0]
-	if !(e2 != nil) {
-		t.Fatal()
-	}
-	if !(len(e2.next) == 1) {
-		t.Fatal()
-	}
-
-	if !(e2.next[0] == nil) {
-		t.Fatal()
-	}
-
-	if !(e1.offset == 0) {
-		t.Fatal()
-	}
-	if !(e1.data == "0123") {
-		t.Fatal()
-	}
-	if !(e2.offset == 4) {
-		t.Fatal()
-	}
-	if !(e2.data == "4567") {
-		t.Fatal()
-	}
+	SanityCheck(t, s, []content{
+		{0, "0123"},
+		{4, "4567"},
+	})
 }
 
-func TestSkipList_InsertSame(t *testing.T) {
+func TestSkipList_02_Empty(t *testing.T) {
+	s := newSkipList(2)
+	SanityCheck(t, s, []content{})
+}
 
-	s := newSkipList(1)
-	s.insert(0, "0123")
+func TestSkipList_02_SingleInsert(t *testing.T) {
+
+	s := newSkipList(2)
 	s.insert(0, "0123")
 
-	e := s.header.next[0]
-	if !(e != nil) {
-		t.Fatal()
-	}
-	if !(len(e.next) == 1) {
-		t.Fatal()
-	}
-	if !(e.next[0] == nil) {
-		t.Fatal()
-	}
-	if !(e.offset == 0) {
-		t.Fatal()
-	}
-	if !(e.data == "0123") {
-		t.Fatal()
-	}
+	SanityCheck(t, s, []content{
+		{0, "0123"},
+	})
+}
+
+func TestSkipList_02_DoubleInsert(t *testing.T) {
+
+	s := newSkipList(2)
+	s.insert(0, "0123")
+	s.insert(4, "4567")
+
+	SanityCheck(t, s, []content{
+		{0, "0123"},
+		{4, "4567"},
+	})
 }
