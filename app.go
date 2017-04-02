65755e75f60907893832fb39932210cf1cbcccea
Peter Stace <peterstace@gmail.com>
2017-04-02 17:11:49 +1000

    Refactor moving to start of file

--- app.go
+++ app.go
@@ -117,34 +117,8 @@ func (a *app) moveUp() {
 }
 
 func (a *app) moveTop() {
-
-	// TODO: Refactor with generic move functions.
-
 	a.log.Info("Jumping to start of file.")
-	if a.offset == 0 {
-		return
-	}
-	haveOffsetZero := false
-	for _, ln := range a.bck {
-		if ln.offset == 0 {
-			haveOffsetZero = true
-		}
-	}
-	if haveOffsetZero {
-		for a.offset != 0 {
-			// TODO: Bad performance, but will be okay once a linked list is used.
-			ln := a.bck[0]
-			a.fwd = append([]line{ln}, a.fwd...)
-			a.bck = a.bck[1:]
-			a.offset = ln.offset
-		}
-	} else {
-		a.fwd = nil
-		a.bck = nil
-		a.offset = 0
-	}
-	a.refresh()
-
+	a.moveToOffset(0)
 }
 
 func (a *app) moveBottom() {
